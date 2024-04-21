e01ca1b57a49624b0f213b7f8494f079ffd77e2f
Hector <hectordesosa.01@gmail.com>
2024-04-21 14:35:16 -0300

    original

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -1,55 +1,26 @@
-import asyncio, ssl, certifi, logging, os
+import asyncio, ssl, certifi, logging
 import aiomqtt
+from environs import Env
 
-#logging.getLogger(__name__)
-logging.basicConfig(format='%(taskName)s: %(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S')
+env = Env()
+env.read_env() #lee el archivo con las variables. por defecto .env
 
-class Contador:
-    def __init__(self):
-        self.__contador = 0 #doble guion bajo privada
-    def incrementar(self):
-        self.__contador += 1
-    def obtener_valor(self):
-        return self.__contador
+logging.basicConfig(format='%(asctime)s - cliente mqtt - %(levelname)s:%(message)s', level=logging.INFO, datefmt='%d/%m/%Y %H:%M:%S')
 
-async def main(client):
-    while True:
-        async with client.messages() as messages:
-            await client.subscribe(os.environ['TOPICO1'])
-            await client.subscribe(os.environ['TOPICO2'])
-            async for message in messages:
-                logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
-        await asyncio.sleep(5)
-async def publicacion(client):
-    while True:
-        await client.publish(os.environ['PUBLICAR'],str(mi_contador.obtener_valor()))#aca publico el contador
-        await asyncio.sleep(5)
-async def contador():
-    while True:
-        mi_contador.incrementar()
-        await asyncio.sleep(3)
-async def master():
+async def main():
     tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     tls_context.verify_mode = ssl.CERT_REQUIRED
     tls_context.check_hostname = True
     tls_context.load_default_certs()
+
     async with aiomqtt.Client(
-        os.environ['SERVIDOR'],
+        env("SERVIDOR"),
         port=8883,
         tls_context=tls_context,
     ) as client:
-        await asyncio.gather(main(client), publicacion(client),contador())
+        await client.subscribe("#")
+        async for message in client.messages:
+            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
 
 if __name__ == "__main__":
-    try:
-        mi_contador = Contador()
-        asyncio.run(master())
-    except KeyboardInterrupt:
-        pass
-
-
-#docker image ls
-#clienteMqtt $ docker image rm nombre -f
-#docker build -t clientemqtt .
-#docker run --rm --name cliente_mqtt clientemqtt
-#docker-compose up --build
+    asyncio.run(main())
